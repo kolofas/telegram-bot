aa8c68d4f985ac1372474d36f350bf779388b229
Nikita <98541680+kolofas@users.noreply.github.com>
2024-03-17 21:13:25 +0500

    Delete choices directory

--- choices/fastfood.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-
-places = ['Макдоли', "КФС", "Бургера Кинга", "ТАКОС НАХУЙ", "ЧИКАН", "ХУЙЛАН", "КАПИТАН"]
-
-
-def pick():
-    """Return random fast food place"""
-    return choice(places)
